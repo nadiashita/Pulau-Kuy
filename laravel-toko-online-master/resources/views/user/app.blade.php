12f850bdb1e72235a1fdb68a22edcb647710c4ac
Nadia Noor Nashita <68615472+nadiashita@users.noreply.github.com>
2021-06-16 14:22:36 +0700

    nambah boostrap  dikit

--- laravel-toko-online-master/resources/views/user/app.blade.php
+++ laravel-toko-online-master/resources/views/user/app.blade.php
@@ -134,7 +134,7 @@
             <li class="{{ Request::path() === '/' ? '' : '' }}"><a href="{{ route('home') }}">Beranda</a></li>
             <li class="{{ Request::path() === 'produk' ? '' : '' }}"><a href="{{ route('user.produk') }}">Paket Wisata</a></li>
             <li class="{{ Request::path() === 'metode_ibcf' ? '' : '' }}"><a href="{{ route('metode_ibcf') }}">METODE iBCF</a></li>
-            <li class="{{ Request::path() === 'peta' ? '' : '' }}"><a href="{{ route('peta') }}">Peta</a></li>
+            <li class="{{ Request::path() === 'peta' ? '' : '' }}"><a href="{{ route('peta') }}">Virtual Tour</a></li>
           </ul>
         </div>
       </nav>
@@ -148,7 +148,7 @@
           <div class="col-lg-6 mb-5 mb-lg-0">
             <div class="row">
               <div class="col-md-12">
-                <h3 class="footer-heading mb-4">Navigations</h3>
+                <!-- <h3 class="footer-heading mb-4">Navigations</h3>
               </div>
               <div class="col-md-6 col-lg-4">
                 <ul class="list-unstyled">
@@ -173,7 +173,7 @@
                 </ul>
               </div>
             </div>
-          </div>
+          </div> -->
           <div class="col-md-6 col-lg-6">
             <div class="block-5 mb-5">
               <h3 class="footer-heading mb-4">Contact Info</h3>
@@ -184,7 +184,7 @@
               </ul>
             </div>
 
-            <div class="block-7">
+            <!-- <div class="block-7">
               <form action="#" method="post">
                 <label for="email_subscribe" class="footer-heading">Subscribe</label>
                 <div class="form-group">
@@ -192,15 +192,18 @@
                   <input type="submit" class="btn btn-sm btn-primary" value="Send">
                 </div>
               </form>
-            </div>
+            </div> -->
           </div>
+          </div>
+          
         </div>
         <div class="row pt-5 mt-5 text-center">
           <div class="col-md-12">
             <p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+            <!-- Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a> -->
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            
             </p>
           </div>
           
